8e0f3e8b35f808b430aabbe3faa9cd00afc3ef2f
ToddPack <rtpack@charter.net>
2023-03-10 23:07:17 -0500

    Startup changes

--- scripts/windowsplay2.bat
+++ scripts/windowsplay2.bat
@@ -1,3 +1,4 @@
-cd c:\gstreamer\1.0\x86_64\bin
+C:
+cd \gstreamer\1.0\msvc_x86_64\bin
 gst-launch-1.0 -v udpsrc port=5802 ! "application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96" ! rtph264depay ! h264parse ! avdec_h264 ! timeoverlay ! autovideosink
 
